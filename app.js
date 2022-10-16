271aab62f924fa6e3cb50c8947663fc44cd2ae07
JerryVW <jerryvw72@gmail.com>
2022-10-16 12:17:07 -0400

    Made a few changes for better looks on mobile, added about page and started working on javascript fumctionality

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const imgZoom = document.querySelector('img');
 const classActive = document.querySelector('.carousel-item');
+const contact = document.querySelector('#services button');
+const about = document.querySelector('#about-button');
+
+
+// Function for images to zoom in and out when clicked
 
 const addTransitionClass = () => {
   if(classActive === 'active') {
@@ -10,3 +15,16 @@ const addTransitionClass = () => {
 }
 
 addTransitionClass();
+
+// Functionality for contact buttons to go to the form
+
+contact.addEventListener("click", function(e) {
+    if(e.target.className == 'header-button') {
+        alert("clicked");
+        // e.target.id = '#contact';
+    }
+});
+
+// else if (e.target == about) {
+//     alert("clicked also");
+//     // e.target.href = '';
