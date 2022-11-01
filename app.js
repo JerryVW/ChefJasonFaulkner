db19e10aeaf6e69c5fe0c1008237f31e1987e125
JerryVW <jerryvw72@gmail.com>
2022-10-31 21:51:00 -0400

    Made some changes for mobile and worked on setting up layout for menu section

--- app.js
+++ app.js
@@ -1,27 +1,18 @@
 window.onload = () => {
-    const imgZoom = document.querySelector('img');
-    const classActive = document.querySelector('.carousel-item');
-    const contactMe = document.querySelectorAll('.header-button');
-    const about = document.querySelector('.exp-button');
-  
-    
-    // Functionality for contact buttons to go to the form
-    
-    Array.from(contactMe).forEach(function(contact){
-        contact.addEventListener("click", function(e) {
-                location.href = '#contact';
-        });
-    
+  const contactMe = document.querySelectorAll(".header-button");
+  const about = document.querySelector(".exp-button");
+
+  // Functionality for contact buttons to go to the form
+
+  Array.from(contactMe).forEach(function (contact) {
+    contact.addEventListener("click", function (e) {
+      location.href = "#contact";
     });
-    
-    if(about) {
-        about.addEventListener('click', function(e) {
-            location.href = 'about.html'
-        });
-    }
+  });
 
-}
-
-
-
-
+  if (about) {
+    about.addEventListener("click", function (e) {
+      location.href = "about.html";
+    });
+  }
+};
